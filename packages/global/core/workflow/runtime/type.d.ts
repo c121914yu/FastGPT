919a7fce5ba2c37597c4a8b9c67f8f43c72a9bfc
heheer <71265218+newfish-cmyk@users.noreply.github.com>
2024-04-23 18:19:27 +0800

    fix if-else node (#87)
    * fix if-else node
    
    * type
    
    * fix

--- packages/global/core/workflow/runtime/type.d.ts
+++ packages/global/core/workflow/runtime/type.d.ts
@@ -74,8 +74,8 @@ export type DispatchNodeResponseType = {
   pluginOutput?: Record<string, any>;
   pluginDetail?: ChatHistoryItemResType[];
 
-  // tf switch
-  tfSwitchResult?: boolean;
+  // if-else
+  ifElseResult?: 'IF' | 'ELSE';
 
   // tool
   toolCallTokens?: number;
